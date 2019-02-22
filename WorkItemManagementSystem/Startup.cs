46b34e1d6cf2835495e72e852abe44c13d2a9492
grozdeto <gros2@abv.bg>
2019-02-22 16:59:24 +0200

    Added and configured Autofac

--- WorkItemManagementSystem/Startup.cs
+++ WorkItemManagementSystem/Startup.cs
@@ -1,14 +1,34 @@
 ﻿namespace WorkItemManagementSystem
 {
+    using Autofac;
     using System;
+    using System.Linq;
+    using System.Reflection;
+    using WorkItemManagementSystem.Commands.Contracts;
     using WorkItemManagementSystem.Core;
+    using WorkItemManagementSystem.Core.Contracts;
 
     class Staratup
     {
         static void Main(string[] args)
         {
-            var engine = Engine.Instance;
+   
+
+            // Commands
+          
+
+            // Data
+
+            // Engine
+            var config = new AutofacConfig();
+            var container = config.Build();
+
+            var engine = container.Resolve<IEngine>();
             engine.Start();
+            
+
+            //var engine = Engine.Instance;
+            //engine.Start();
         }
     }
 }
